d05a1ef729b2a1e3e37bcc22831c5821ec14e298
ttikke <ttikke@ttu.ee>
2023-01-30 12:18:01 +0200

    removed unnecessary elements

--- nullam-front/src/components/__tests__/AddCorporationFromTests.test.js
+++
@@ -1,6 +0,0 @@
-import AddCorporationForm from "../AddCorporationForm.js";
-import {render} from "@testing-library/react";
-
-test('renders without crashing', () => {
-    render(<AddCorporationForm/>)
-})
